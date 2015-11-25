f83043519fd54dd2b5fe3e67a9e82fc0eeb483c1
matiascanodesarrollos <matiascanodesarrollos@matiascanodesarrollos>
2015-11-24 21:16:36 -0300

    Puntaje y Visita

---
+++ ProyectoTurismo/src/java/turismo/controlador/servlet/AltaVisita.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package turismo.controlador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import turismo.entidades.Fechas;
+import turismo.entidades.Visita;
+
+/**
+ *
+ * @author matiascanodesarrollos
+ */
+public class AltaVisita extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String fecha = request.getParameter("fechaVisita");
+        String hora = request.getParameter("horaVisita");
+        int lugar = -1;
+        if(!request.getParameter("lugarVisita").isEmpty())
+            lugar = Integer.parseInt(request.getParameter("lugarVisita"));
+        int viaje = -1;
+        if(!request.getParameter("idViajeVisita").isEmpty())
+            viaje = Integer.parseInt(request.getParameter("idViajeVisita"));
+        int estado = -1;
+        if(!request.getParameter("estadoVisita").isEmpty())
+            estado = Integer.parseInt(request.getParameter("estadoVisita"));        
+        try {
+            /* TODO output your page here. You may use following sample code. */
+            if(!fecha.isEmpty() && !hora.isEmpty() && lugar != -1 && viaje != -1 && estado != -1){
+                new Visita(fecha,hora,Fechas.fechaActual(),viaje,estado);
+                out.println("Visita cargada.");
+            }else{
+                out.println("Todos los campos son obligatorios.");
+            }
+        } catch (SQLException ex) {
+            out.println(ex.toString());
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Error de Metodo</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Estimado usuario, para su seguridad utilice la interfaz brindada.</h1>");
+        out.println("</body>");
+        out.println("</html>");
+        out.close();
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ ProyectoTurismo/src/java/turismo/entidades/Visita.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package turismo.entidades;
+
+import java.sql.SQLException;
+
+/**
+ *
+ * @author matiascanodesarrollos
+ */
+public class Visita extends ObjetoPuntuable {
+    private int id;
+    private String fecha;
+    private String hora;
+    private String fechaCarga;
+    private int viaje;
+    private int estado;
+    
+    //Constructor
+
+    public Visita(String fecha, String hora, String fechaCarga, int viaje, int estado) throws SQLException {
+        this.fecha = fecha;
+        this.hora = hora;
+        this.fechaCarga = fechaCarga;
+        this.viaje = viaje;
+        this.estado = estado;
+        generarObjetoParaBD();
+        getCon().getSql().executeUpdate("INSERT INTO Turismo.Visita (fecha,hora,fechaCarga,IdViaje,IdEstado,idObjetoPuntuable) values ('" + fecha +"','" + hora + "','" + fechaCarga + "','" + viaje + "','" + estado + getIdObjeto()+"')");
+        cerrarConexion();
+    }
+    
+}
